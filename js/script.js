8520bab012f0ed00f09078a5478c07862dfcc154
Rielees777 <39187124+Rielees777@users.noreply.github.com>
2018-12-16 19:15:10 +0300

    изменен js

--- js/script.js
+++ js/script.js
@@ -1,52 +1,53 @@
-//var feedback_link = document.querySelector(".contact-us");
-//var feedback_popup = document.querySelector(".modal-feedback");
-//var feedback_close = feedback_popup.querySelector(".modal-close");
-//
-//var map_link = document.querySelector(".image-map");
-//var map_popup = document.querySelector(".modal-map");
-//var map_close = map_popup.querySelector(".modal-close")
+var feedback_link = document.querySelector(".contact-us");
+var feedback_popup = document.querySelector(".modal-feedback");
+var feedback_close = feedback_popup.querySelector(".modal-close");
 
+var map_link = document.querySelector(".image-map");
+var map_popup = document.querySelector(".modal-map");
+var map_close = map_popup.querySelector(".modal-close")
 
-//var basket_add_close = basket_add_popup.querySelector(".modal-close");
-
-//feedback_link.addEventListener("click", function(evt){
-//  evt.preventDefault();
-//  feedback_popup.classList.add("modal-show");
-//
-//});
-//
-//feedback_close.addEventListener("click", function(evt){
-//  evt.preventDefault();
-//  feedback_popup.classList.remove("modal-show");
-//});
-//document.addEventListener("keydown", function(evt){
-//  if (evt.keyCode === 27) {
-//    evt.preventDefault();
-//    if (feedback_popup.classList.contains("modal-show")){
-//    feedback_popup.classList.remove("modal-show");
-//    }
-//  }
-//});
-//
-//
-//map_link.addEventListener("click", function(evt){
-//  evt.preventDefault();
-//  map_popup.classList.add("modal-show");
-//});
-//map_close.addEventListener("click", function(evt){
-//  evt.preventDefault();
-//  map_popup.classList.remove("modal-show");
-//});
-//document.addEventListener("keydown", function(evt){
-//  if (evt.keyCode === 27) {
-//    evt.preventDefault();
-//    if (map_popup.classList.contains("modal-show")){
-//    map_popup.classList.remove("modal-show");
-//    }
-//  }
-//});
-var basket_link = document.querySelector(".catalog");
+var basket_link = document.querySelector(".catalog-list");
 var basket_popup = document.querySelector(".modal-add-basket");
+var basket_close = basket_popup.querySelector(".modal-close");
+
+feedback_link.addEventListener("click", function(evt){
+  evt.preventDefault();
+  feedback_popup.classList.add("modal-show");
+
+});
+
+feedback_close.addEventListener("click", function(evt){
+  evt.preventDefault();
+  feedback_popup.classList.remove("modal-show");
+});
+document.addEventListener("keydown", function(evt){
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (feedback_popup.classList.contains("modal-show")){
+    feedback_popup.classList.remove("modal-show");
+    }
+  }
+});
+
+
+map_link.addEventListener("click", function(evt){
+  evt.preventDefault();
+  map_popup.classList.add("modal-show");
+});
+map_close.addEventListener("click", function(evt){
+  evt.preventDefault();
+  map_popup.classList.remove("modal-show");
+});
+document.addEventListener("keydown", function(evt){
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (map_popup.classList.contains("modal-show")){
+    map_popup.classList.remove("modal-show");
+    }
+  }
+});
+
+
 
 basket_link.addEventListener("click", function(evt){
 
@@ -54,16 +55,16 @@ basket_link.addEventListener("click", function(evt){
 
 });
 
+basket_close.addEventListener("click", function(evt){
+  evt.preventDefault();
+  basket_popup.classList.remove("modal-show");
+});
 
-//basket_add_close.addEventListener("click", function(evt){
-//  evt.preventDefault();
-//  basket_add_popup.classList.remove("modal-show");
-//});
-//document.addEventListener("keydown", function(evt){
-//  if (evt.keyCode === 27) {
-//    evt.preventDefault();
-//    if (basket_add_popup.classList.contains("modal-show")){
-//    basket_add_popup.classList.remove("modal-show");
-//    }
-//  }
-//});
+document.addEventListener("keydown", function(evt){
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (basket_popup.classList.contains("modal-show")){
+    basket_popup.classList.remove("modal-show");
+    }
+  }
+});
